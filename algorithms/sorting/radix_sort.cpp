5fc801570b5ffd3f51ef0d11ef51245b1fb63b3f
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-21 17:54:39 +0200

    working version of radix_sort

--- algorithms/sorting/radix_sort.cpp
+++ algorithms/sorting/radix_sort.cpp
@@ -1,5 +1,6 @@
 // RADIX SORT
-// implementation of radix sort, using counting sort (stable) under the hood.
+// implementation of radix sort, using counting sort (stable) under the hood. No negative
+// values allowed. Reused some helper functions and modified my counting sort algorithm.
 //
 // RUNTIME
 //    best-case: O() -> 
@@ -24,6 +25,87 @@ int toPowerOf(int number, int power_of)
   }
 
   return output;
+}
+
+// helper function to fill array with given integer
+void fillArrayWith(int arr[], int arr_size, int fill)
+{
+  for (int i = 0; i < arr_size; i++)
+  {
+    arr[i] = fill;
+  }
+}
+
+// this version of counting sort is very similar to the one I already wrote in counting_sort.cpp,
+// yet is slightly modified to match the new requirements. It does not determine the smallest value
+// and uses a range from 0 up to the largest digit instead.
+void countingSort(int arr[], int arr_size, int divider)
+{
+  // find largest digit in array
+  int largest_digit = 0;
+  int num = 0;
+
+  for (int i = 0; i < arr_size; i++)
+  {
+    num = (arr[i] / divider) % 10;
+
+    if (num > largest_digit)
+    {
+      largest_digit = num;
+    }
+  }
+
+  // create temporary array, initialized with 0s
+  int temp_size = largest_digit + 1;
+  int arr_temp[temp_size];
+
+  // fill array with 0s
+  fillArrayWith(arr_temp, temp_size, 0);
+
+  // count number of occurrences
+  for (int i = 0; i < arr_size; i++)
+  {
+    num = (arr[i] / divider) % 10;
+    arr_temp[num]++;
+  }
+
+  // compute prefix sums
+  for (int i = 1; i < temp_size; i++)
+  {
+    arr_temp[i] += arr_temp[i - 1];
+  }
+
+  // shift array one index to the right, emitting value on last index
+  for (int i = largest_digit; i >= 1; i--)
+  {
+    arr_temp[i] = arr_temp[i - 1];
+  }
+
+  // reset value at index 0
+  arr_temp[0] = 0;
+
+  // we create a copy of our source array
+  int arr_copy[arr_size];
+
+  for (int i = 0; i < arr_size; i++)
+  {
+    arr_copy[i] = arr[i];
+  }
+
+  // populate the source array with sorted values using our array copy 
+  // as well as information about each starting index 
+  for (int i = 0; i < arr_size; i++)
+  {
+    // get starting index
+    num = (arr_copy[i] / divider) % 10;
+    int index = arr_temp[num];
+
+    // add value in correct position
+    arr[index] = arr_copy[i];
+
+    // increase position
+    arr_temp[num]++;
+  }
 }
 
 void radixSort(int arr[], int arr_size)
@@ -55,15 +137,8 @@ void radixSort(int arr[], int arr_size)
     int divider = 1;
     divider = toPowerOf(10, digit);
 
-    for (int j = 0; j < arr_size; j++)
-    {
-      int num = (arr[j] / divider) % 10;
-
-      // counting sort
-    }
+    countingSort(arr, arr_size, divider);
   }
-
-  
 }
 
 void printArray(const int arr[], int arr_size)
@@ -78,8 +153,7 @@ void printArray(const int arr[], int arr_size)
 
 int main()
 {
-  // int arr[] = {5, 2, 4, 9, -2, 13, 1, 3, 100, 232, 67, 50, 0, 3, -40, -10, 20};
-  int arr[] = {343, 567, 819, 199, 201, 506, 784};
+  int arr[] = {343, 567, 819, 199, 201, 506, 784, 111, 12, 99, 3};
   int arr_size = sizeof(arr) / sizeof(arr[0]);
 
   std::cout << "unsorted: ";
