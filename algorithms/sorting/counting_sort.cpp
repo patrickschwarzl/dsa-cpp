143bc801b1fb72a4fa933284724a5edd0fae4aff
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-22 10:42:39 +0200

    counting_sort: fixed warnings (except VLAs)

--- algorithms/sorting/counting_sort.cpp
+++ algorithms/sorting/counting_sort.cpp
@@ -14,9 +14,9 @@
 #include <iostream>
 
 // fills array with given integer
-void fillArrayWith(int arr[], int arr_size, int fill)
+void fillArrayWith(int arr[], std::size_t arr_size, int fill)
 {
-  for (int i = 0; i < arr_size; i++)
+  for (std::size_t i = 0; i < arr_size; i++)
   {
     arr[i] = fill;
   }
@@ -25,7 +25,7 @@ void fillArrayWith(int arr[], int arr_size, int fill)
 // a version of counting sort which only operates on positive values and also assumes the range
 // starts at 0, therefore not needing to determine the smallest value or having to shift the values.
 // (strictly ignoring VLA not being supported by C++)
-void countingSort(int arr[], int arr_size)
+void countingSort(int arr[], std::size_t arr_size)
 {
   // nothing to sort
   if (arr_size <= 1)
@@ -34,7 +34,7 @@ void countingSort(int arr[], int arr_size)
   // find the largest integer in the given array
   int target = 0;
 
-  for (int i = 0; i < arr_size; i++)
+  for (std::size_t i = 0; i < arr_size; i++)
   {
     if (arr[i] > target)
     {
@@ -43,23 +43,23 @@ void countingSort(int arr[], int arr_size)
   }
 
   // create a temporary array, initialized with 0 at every index
-  int temp_size = target + 1;
+  std::size_t temp_size = target + 1;
   int arr_temp[temp_size];
 
   // fill array with 0s
   fillArrayWith(arr_temp, temp_size, 0);
 
   // count number of occurrences
-  int num = 0;
+  std::size_t num = 0;
 
-  for (int i = 0; i < arr_size; i++)
+  for (std::size_t i = 0; i < arr_size; i++)
   {
     num = arr[i];
     arr_temp[num]++;
   }
 
   // compute prefix sums
-  for (int i = 1; i < temp_size; i++)
+  for (std::size_t i = 1; i < temp_size; i++)
   {
     arr_temp[i] += arr_temp[i - 1];
   }
@@ -77,14 +77,14 @@ void countingSort(int arr[], int arr_size)
   // we create a copy of our source array
   int arr_copy[arr_size];
 
-  for (int i = 0; i < arr_size; i++)
+  for (std::size_t i = 0; i < arr_size; i++)
   {
     arr_copy[i] = arr[i];
   }
 
   // populate the source array with sorted values using our array copy 
   // as well as information about each starting index 
-  for (int i = 0; i < arr_size; i++)
+  for (std::size_t i = 0; i < arr_size; i++)
   {
     // get starting index
     int index = arr_temp[arr_copy[i]];
@@ -97,7 +97,7 @@ void countingSort(int arr[], int arr_size)
   }
 }
 
-void printArray(const int arr[], int arr_size)
+void printArray(const int arr[], std::size_t arr_size)
 {
   for (std::size_t i = 0; i < arr_size; ++i)
   {
@@ -111,7 +111,7 @@ int main()
 {
   // here we have a relatively small range of 0 to 9
   int arr[] = {3, 1, 5, 9, 2, 0, 2, 1, 1, 9, 5, 3};
-  int arr_size = sizeof(arr) / sizeof(arr[0]);
+  std::size_t arr_size = sizeof(arr) / sizeof(arr[0]);
 
   std::cout << "unsorted: ";
   printArray(arr, arr_size);
