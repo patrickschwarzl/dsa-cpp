3bc27d0e0fce80628d5b3b1868f126e1c98e2f7b
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-23 20:21:47 +0200

    working version of static_stack

--- data-structures/static_stack.cpp
+++ data-structures/static_stack.cpp
@@ -1,6 +1,6 @@
 // STATIC STACK
 // stack data-structure with fixed compile-time capacity. Fully stack allocated 
-// with no dynamic heap allocations.
+// with no dynamic heap allocations. Features basic stack operations.
 
 #include <iostream>
 
@@ -14,68 +14,157 @@ class Stack
     // members
     T arr_[Capacity];
     std::size_t size_;
-    bool capacity_reached_;
-
-    // private methods
-    const bool checkCapacity()
-    {
-      if (size_ >= Capacity)
-      {
-        capacity_reached_ = true;
-        return false;
-      }
-
-      capacity_reached_ = false;
-      return true;
-    }
-
-    void increaseSize()
-    {
-      size_++;
-      checkCapacity();
-    }
 
   public:
-    // constructor
-    Stack() : size_(0), capacity_reached_(false) {}
+    /// ---------------------------------------------------------------------------------------------------------------
+    /**
+     * @brief Constructor.
+     */
+    Stack() : size_(0) {}
 
+    /**
+     * @brief Copy constructor.
+     */
+    Stack(const Stack&) = default;
+
+    /**
+     * @brief Move constructor.
+     */
+    Stack(Stack&&) = default;
+
+    /**
+     * @brief Copy assignment operator.
+     */
+    Stack& operator=(const Stack&) = default;
+    
+    /**
+     * @brief Move assignment operator.
+     */
+    Stack& operator=(Stack&&) = default;
+
+    /// ---------------------------------------------------------------------------------------------------------------
     // basic operations
 
-    // @brief returns top element
-    // user is responsible for undefined behavior
+    /**
+     * @brief Returns a read-only reference version of the top element.
+     * @warning User is responsible for avoiding undefined behavior.
+     * @return const T& 
+     */
     const T &top() const
     {
       return arr_[size_ - 1];
     }
 
-    // @brief non-const version, returns top element
+    /**
+     * @brief Returns a non-const reference version of the top element.
+     * @warning User is responsible for avoiding undefined behavior.
+     * @return T& 
+     */
     T &top()
     {
       return arr_[size_ - 1];
     }
 
-    // pushes element into stack
+    /**
+     * @brief Pushes element on top of stack.
+     * @param element 
+     */
     void push(const T &element)
     {
-      if (!checkCapacity())
+      if (reachedCapacity())
       {
-        std::cout << "ERROR: Maximum capacity reached. Element couldn't be added!" << std::endl;
+        std::cout << "ERROR: Maximum capacity reached. Element couldn't be added!\n";
         return;
       }
 
       arr_[size_] = element;
-      increaseSize();
+      ++size_;
+    }
+
+    /**
+     * @brief Pops last element from stack.
+     */
+    void pop()
+    {
+      if (!empty())
+      {
+        --size_;
+        return;
+      }
+
+      std::cout << "ERROR: Stack underflow, cannot pop from empty stack!\n";
+    }
+
+    /**
+     * @brief Returns true if the stack is empty, false otherwise.
+     * @return Boolean value
+     */
+    bool empty() const
+    {
+      return size_ == 0;
+    }
+
+    /**
+     * @brief Resets the stack.
+     */
+    void clear()
+    {
+      size_ = 0;
+    }
+
+    /**
+     * @brief Returns the stacks capacity.
+     * @return std::size_t 
+     */
+    std::size_t capacity() const
+    {
+      return Capacity;
+    }
+
+    /**
+     * @brief Returns true if capacity limit has been reached, false otherwise.
+     * @return True if capacity was reached, false otherwise.
+     */
+    bool reachedCapacity() const
+    {
+      return size_ == Capacity;
+    }
+
+    /**
+     * @brief Returns the current size of the stack.
+     * @return std::size_t 
+     */
+    std::size_t size() const
+    {
+      return size_;
     }
 };
 
 int main()
 {
-  Stack<int, 20> s;
+  // initialized new stack
+  Stack<int, 3> s;
 
+  // add elements to stack
   s.push(2);
   s.push(4);
+  s.push(3);
+  s.push(10); // blocked as capacity had been reached
 
-  std::cout << s.top() << std::endl;
+  // pop last element
+  s.pop();
+
+  std::cout << s.top() << std::endl; // 4
+  std::cout << "size: " << s.size() << std::endl; // 2
+
+  // reset stack
+  s.clear();
+
+  // add element
+  s.push(6);
+
+  std::cout << s.top() << std::endl; // 6
+  std::cout << "size: " << s.size() << std::endl; // 1
 
   return 0;
 }
