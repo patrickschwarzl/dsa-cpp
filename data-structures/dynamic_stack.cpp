763263b4c9907aaf44c7f98c32e64dececc1c181
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-24 15:50:31 +0200

    implemented push() operation

---
+++ data-structures/dynamic_stack.cpp
@@ -0,0 +1,73 @@
+// DYNAMIC STACK
+// stack data-structure which allocates elements dynamically on the heap. Fully
+// heap allocated and features basic stack operations.
+//
+// TIME COMPLEXITY
+//
+
+#include <iostream>
+#include <memory>
+#include <optional>
+
+const constexpr std::size_t CAPACITY = 10;
+const constexpr std::size_t CAPACITY_INTERVAL = 5;
+
+template <typename T> class Stack
+{
+  private:
+    std::unique_ptr<T[]> arr_;
+    std::size_t capacity_;
+    std::optional<std::size_t> index_;
+
+  public:
+    // Constructor
+    Stack()
+        : capacity_(10), arr_(std::make_unique<T[]>(CAPACITY)),
+          capacity_(CAPACITY), index_(std::nullopt)
+    {
+    }
+
+    // Destructor, although the arr_ memory block is freed by the unique
+    // pointer.
+    ~Stack() = default;
+
+    /**
+     * @brief Move constructor.
+     */
+    Stack(Stack &&) = default;
+
+    /**
+     * @brief Copy assignment operator.
+     */
+    Stack &operator=(const Stack &) = delete;
+
+    /**
+     * @brief Move assignment operator.
+     */
+    Stack &operator=(Stack &&) = delete;
+
+    // basic operations
+    void push(const T &element)
+    {
+      if (!index_.has_value())
+        index_ = 0;
+      else
+        index_++;
+
+      // check if additional memory needs to be allocated
+      if (index_ >= capacity_)
+      {
+        capacity_ += CAPACITY_INTERVAL;
+        arr_ = std::make_unique<T[]>(capacity_);
+      }
+
+      arr_[index_] = element;
+    }
+};
+
+int main()
+{
+  Stack<int> s;
+
+  return 0;
+}
