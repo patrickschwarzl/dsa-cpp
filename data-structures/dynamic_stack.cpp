c5a3ce78866ed2a46ac5e27a48279f682df6ef33
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-24 19:06:55 +0200

    finished dynamic_stack

--- data-structures/dynamic_stack.cpp
+++ data-structures/dynamic_stack.cpp
@@ -3,6 +3,15 @@
 // heap allocated and features basic stack operations.
 //
 // TIME COMPLEXITY
+//  top()             -> O(1)
+//  push()            -> O(1) amortized | O(n) in worst-case
+//  pop()             -> O(1)
+//  empty()           -> O(1)
+//  clear()           -> O(1)
+//  capacity()        -> O(1)
+//  reachedCapacity() -> O(1)
+//  size()            -> O(1)
+//  shrinkToSize()    -> O(n)
 //
 
 #include <iostream>
@@ -49,6 +58,11 @@ template <typename T> class Stack
     // basic operations
 
     //-----------------------------------------------------------------------------------------------------------------
+    /// @brief Returns the stacks current size.
+    /// @return std::size_t
+    std::size_t size() const { return index_.has_value() ? *index_ + 1 : 0; }
+
+    //-----------------------------------------------------------------------------------------------------------------
     /// @brief Pushes element on top of stack.
     /// @param element
     void push(const T &element)
@@ -62,7 +76,20 @@ template <typename T> class Stack
       if (*index_ >= capacity_)
       {
         capacity_ += CAPACITY_INTERVAL;
-        arr_ = std::make_unique<T[]>(capacity_);
+
+        // allocate new memory
+        std::unique_ptr<T[]> new_arr = std::make_unique<T[]>(capacity_);
+
+        // safely move elements from source array into the new array
+        for (std::size_t i = 0; i < *index_; i++)
+        {
+          new_arr[i] = std::move(arr_[i]);
+        }
+
+        // replace the old array with the new one
+        arr_ = std::move(new_arr);
+
+        std::cout << "INFO: Allocated more size!\n";
       }
 
       arr_[*index_] = element;
@@ -84,6 +111,60 @@ template <typename T> class Stack
 
       std::cout << "ERROR: Stack underflow, cannot pop from empty stack!\n";
     }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// @brief Shrink the stack to it's current size.
+    void shrinkToSize()
+    {
+      std::size_t current_size = size();
+      std::size_t target_capacity = current_size > 0 ? current_size : 1;
+
+      if (target_capacity != capacity_)
+      {
+        // allocate new memory
+        std::unique_ptr<T[]> new_arr = std::make_unique<T[]>(target_capacity);
+        capacity_ = target_capacity;
+
+        // safely move elements from source array into the new array
+        for (std::size_t i = 0; i < current_size; i++)
+        {
+          new_arr[i] = std::move(arr_[i]);
+        }
+
+        // replace the old array with the new one
+        arr_ = std::move(new_arr);
+      }
+      else
+      {
+        std::cout << "INFO: Stack already has optimal size!\n";
+      }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// @brief Returns true if stack is empty.
+    /// @return Boolean value
+    bool empty() const { return !index_.has_value(); }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// @brief Returns the array to its default state, empty and default capacity.
+    void clear()
+    {
+      // reset the stacks capacity to default
+      arr_ = std::make_unique<T[]>(CAPACITY);
+      capacity_ = CAPACITY;
+
+      index_ = std::nullopt;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// @brief Return the stacks current capacity.
+    /// @return std::size_t
+    std::size_t capacity() const { return capacity_; }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// @brief Returns true if capacity is reached, false otherwise.
+    /// @return Boolean value
+    bool reachedCapacity() const { return (size() == capacity_); }
 
     //-----------------------------------------------------------------------------------------------------------------
     /// @brief Returns a read-only reference version of the top element.
@@ -110,6 +191,18 @@ int main()
   s.pop();
 
   std::cout << s.top() << "\n";
+  std::cout << "size: " << s.size() << "\n";
+
+  s.shrinkToSize();
+  s.push(10);
+
+  std::cout << "size: " << s.size() << "\n";
+  std::cout << s.top() << "\n";
+
+  s.clear();
+  
+  std::cout << "size: " << s.size() << "\n";
+  std::cout << "capacity: " << s.capacity() << "\n";
 
   return 0;
 }
