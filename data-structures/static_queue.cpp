ddd2bddaca51d6e4efe9c818c56d08485e3c7140
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-26 20:01:21 +0200

    added dequeue() and introduced sample output

--- data-structures/static_queue.cpp
+++ data-structures/static_queue.cpp
@@ -31,11 +31,28 @@ template <typename T, std::size_t Capacity> class Queue
       ++size_;
     }
 
+    void dequeue()
+    {
+      if (size_ == 0)
+        return;
+
+      if (size_ == 1)
+        size_ = 0;
+
+      // remove element at arr_[0] and shift array to the left
+      for (std::size_t i = 0; i < size_; i++)
+      {
+        arr_[i] = arr_[i + 1];
+      }
+
+      --size_;
+    }
+
     bool capacityReached() const { return size_ == Capacity; }
 
-    T &front() const { return arr_[0]; }
+    T &front() { return arr_[0]; }
 
-    T &back() const { return arr_[size_ - 1]; }
+    T &back() { return arr_[size_ - 1]; }
 
     bool empty() const { return size_ == 0; }
 
@@ -46,4 +63,26 @@ template <typename T, std::size_t Capacity> class Queue
     void clear() { size_ = 0; }
 };
 
-int main() { return 0; }
+int main()
+{
+
+  Queue<int, 3> s;
+
+  s.enqueue(4);
+
+  std::cout << "front: " << s.front() << " back: " << s.back() << "\n";
+
+  std::cout << "size: " << s.size() << "\n";
+
+  s.enqueue(5);
+
+  std::cout << "front: " << s.front() << " back: " << s.back() << "\n";
+
+  s.dequeue();
+
+  std::cout << "size: " << s.size() << "\n";
+
+  std::cout << "front: " << s.front() << " back: " << s.back() << "\n";
+
+  return 0;
+}
