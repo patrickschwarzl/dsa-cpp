d55d7fa3dbe71bfec85b7ccb5babcda49f211753
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-30 14:02:07 +0200

    initial binary tree structure

---
+++ data-structures/binary_tree.cpp
@@ -0,0 +1,22 @@
+// BINARY TREE
+// a binary tree implementation that features all basic operations. Nodes are allocated
+// entirely on the heap.
+//
+// TIME COMPLEXITY
+//
+
+#include <iostream>
+#include <memory>
+
+template<typename T>
+class Tree
+{
+
+};
+
+int main()
+{
+
+
+  return 0;
+}
