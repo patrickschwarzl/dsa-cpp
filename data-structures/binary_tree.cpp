e04ef491b811f2dca8e6e1849f490a6fdfd0907c
Patrick Schwarzl <pacoding199@gmail.com>
2026-07-30 14:13:50 +0200

    syncing devices

--- data-structures/binary_tree.cpp
+++ data-structures/binary_tree.cpp
@@ -1,6 +1,7 @@
 // BINARY TREE
-// a binary tree implementation that features all basic operations. Nodes are allocated
+// a class oriented binary tree implementation that features all basic operations. Nodes are allocated
 // entirely on the heap.
+// valid datatypes include std::size_t, int, double, float.
 //
 // TIME COMPLEXITY
 //
@@ -8,15 +9,31 @@
 #include <iostream>
 #include <memory>
 
-template<typename T>
+template <typename T>
 class Tree
 {
+  private:
+    Tree *root_;
+    Tree *child_left_;
+    Tree *child_right_;
 
+  public:
+    // Constructor
+    Tree()
+        : root_(std::nullopt), child_left_(std::nullopt),
+          child_right_(std::nullopt)
+    {
+    }
+
+    // basic operations
+
+    void addNode(const T &element)
+    {
+      if (root_ == std::nullopt)
+      {
+        // we are at the trees root node
+      }
+    }
 };
 
-int main()
-{
-
-
-  return 0;
-}
+int main() { return 0; }
