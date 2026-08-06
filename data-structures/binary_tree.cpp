bf47529faf78b8c3a722549fc2f331adec3f36ec
Patrick Schwarzl <pacoding199@gmail.com>
2026-08-06 14:27:03 +0200

    syncing devices: brute force approach, much refactoring needed

--- data-structures/binary_tree.cpp
+++ data-structures/binary_tree.cpp
@@ -1,8 +1,9 @@
 // BINARY TREE
-// a class oriented binary tree implementation that features all basic operations. Nodes are allocated
-// entirely on the heap. It also features a frequency count to handle duplicate values instead of allocating
-// multiple nodes with the same value.
-// valid datatypes include std::size_t, int, double, float.
+// a class oriented binary tree implementation that features all basic
+// operations. Nodes are allocated entirely on the heap. It also features a
+// frequency count to handle duplicate values instead of allocating multiple
+// nodes with the same value. valid datatypes include std::size_t, int, double,
+// float.
 //
 // TIME COMPLEXITY
 //
@@ -10,201 +11,184 @@
 #include <iostream>
 #include <memory>
 
-template <typename T>
-class Tree
-{
-  private:
-    const T value_;
-    std::size_t count_;
-    Tree<T> *root_;
-    std::unique_ptr<Tree<T>> child_left_;
-    std::unique_ptr<Tree<T>> child_right_;
+template <typename T> class Tree {
+private:
+  const T value_;
+  std::size_t count_;
+  Tree<T> *root_;
+  std::unique_ptr<Tree<T>> child_left_;
+  std::unique_ptr<Tree<T>> child_right_;
 
-  public:
-    // Constructor
-    Tree(const T &element)
-        : value_(element), count_(1), root_(nullptr), child_left_(nullptr),
-          child_right_(nullptr)
-    {
-    }
+public:
+  // Constructor
+  Tree(const T &element)
+      : value_(element), count_(1), root_(nullptr), child_left_(nullptr),
+        child_right_(nullptr) {}
 
-    // basic operations
+  // basic operations
 
-    void addNode(const T &element)
-    {
-      if (element == value_)
-      {
-        // element with same value already exists, therefore we just increase it's count
-        count_++;
+  void addNode(const T &element) {
+    if (element == value_) {
+      // element with same value already exists, therefore we just increase it's
+      // count
+      count_++;
+      return;
+    } else if (element < value_) {
+      // left subtree
+      if (!child_left_) {
+        // correct node found, allocate new node and initialize its value
+        child_left_ = std::make_unique<Tree<T>>(element);
+        child_left_->root_ = this;
         return;
       }
-      else if (element < value_)
-      {
-        // left subtree
-        if (!child_left_)
-        {
-          // correct node found, allocate new node and initialize its value
-          child_left_ = std::make_unique<Tree<T>>(element);
-          child_left_->root_ = this;
-          return;
-        }
 
-        child_left_->addNode(element);
-      }
-      else
-      {
-        // right subtree
-        if (!child_right_)
-        {
-          child_right_ = std::make_unique<Tree<T>>(element);
-          child_right_->root_ = this;
-          return;
-        }
-
-        child_right_->addNode(element);
-      }
-    };
-
-    bool deleteNode(const T &element)
-    {
-      Tree<T>* node = findNode(element);
-
-      if (!node)
-      {
-        std::cout << "Error: Failed to find Node.\n";
-        return false;
+      child_left_->addNode(element);
+    } else {
+      // right subtree
+      if (!child_right_) {
+        child_right_ = std::make_unique<Tree<T>>(element);
+        child_right_->root_ = this;
+        return;
       }
 
-      std::size_t count = node->count_;
+      child_right_->addNode(element);
+    }
+  };
 
-      if (count > 1)
-      {
-        // simply decrease frequency counter
-        --count;
-        return true;
-      }
+  bool deleteNode(const T &element) {
+    Tree<T> *target_node = findNode(element);
 
-      if (node.get() == this)
-      {
-        // we do not support the deletion of the root node
-        std::cout << "Error: Deleting the trees root node is not allowed.\n";
-        return false;
-      }
+    if (!target_node) {
+      std::cout << "Error: Failed to find Node.\n";
+      return false;
+    }
 
-      // delete Node
-      // we temporarily store the nodes variables 
-      std::unique_ptr<Tree<T>> target_left_child = node->left_child_ ? std::move(node->left_child_) : nullptr;
-      std::unique_ptr<Tree<T>> target_right_child = node->right_child_ ? std::move(node->right_child_) : nullptr;
-      Tree<T>* target_root = node->root_;
+    std::size_t count = target_node->count_;
 
-      // determine if our target node is the left or right child of it's root
-      bool is_left = false;
+    if (count > 1) {
+      // simply decrease frequency counter
+      --count;
+      return true;
+    }
 
-      // we split this process into 3 separate cases.
-      // case 1: target has no children
-      if (!target_left_child && !target_right_child)
-      {
-        // simply delete the Node
-        if (is_left)
-        {
-          target_root->child_left_.reset();
-        }
-        else
-        {
-          target_root->child_right_.reset();
-        }
+    if (target_node == this) {
+      // we do not support the deletion of the root node
+      std::cout << "Error: Deleting the trees root node is not allowed.\n";
+      return false;
+    }
 
-        return true;
-      }
+    // delete Node
+    // we temporarily store the nodes variables
+    std::unique_ptr<Tree<T>> target_left_child =
+        target_node->child_left_ ? std::move(target_node->child_left_)
+                                 : nullptr;
+    std::unique_ptr<Tree<T>> target_right_child =
+        target_node->child_left_ ? std::move(target_node->child_left_)
+                                 : nullptr;
+    Tree<T> *target_root = target_node->root_;
 
-      // case 2: target has a left child + a right child
-      if (target_left_child && target_right_child)
-      {
-        // find the largest value on the left side of the target node
-        // TODO
+    // determine if our target node is the left or right child of it's root
+    bool is_left = false;
 
-        return true;
-      }
-
-      // case 3: target has just one child
-      if (target_left_child)
-      {
-        if (is_left)
-        {
-          target_root->left_child_ = std::move(target_left_child);
-        }
-        else
-        {
-          target_root->right_child_ = std::move(target_left_child);
-        }
-      }
-      else
-      {
-        if (is_left)
-        {
-          target_root->left_child_ = std::move(target_right_child);
-        }
-        else
-        {
-          target_root->right_child_ = std::move(target_right_child);
-        }
+    // we split this process into 3 separate cases.
+    // case 1: target has no children
+    if (!target_left_child && !target_right_child) {
+      // simply delete the Node
+      if (is_left) {
+        target_root->child_left_.reset();
+      } else {
+        target_root->child_right_.reset();
       }
 
       return true;
-
     }
 
-    Tree<T>* findNode(const T &element) 
-    {
-      // return current Node
-      if (element == value_)
-      {
-        return this;
-      }
-      
-      // search left branch
-      if (child_left_ && element < value_)
-      {
-        return child_left_->findNode(element);
+    // case 2: target has a left child + a right child
+    if (target_left_child && target_right_child) {
+      // find predecessor of target node
+      std::unique_ptr<Tree<T>> predecessor = std::move(target_left_child);
+
+      while (predecessor->child_right_) {
+        predecessor = std::move(predecessor->child_right_);
       }
 
-      // search right branch
-      if (child_right_ && element > value_)
-      {
-        return child_right_->findNode(element);
+      // we want to replace the target node with it's predecessor
+      Tree *predecessor_root = predecessor->root_;
+
+      predecessor->root_ = target_root;
+      predecessor->child_left_ = std::move(target_left_child);
+      predecessor->child_right_ = std::move(target_right_child);
+
+      if (is_left) {
+        target_root->child_left_ = std::move(predecessor);
+      } else {
+        target_root->child_right_ = std::move(predecessor);
       }
-      
-      // failed to find Node, return nullptr as fallback value
-      return nullptr;
+
+      predecessor_root->child_right_.reset();
+
+      return true;
     }
 
-    // prints the tree using preorder traversal
-    void printTree() const
-    {
-      // print current value, possible also duplicates
-      for (std::size_t i = 0; i < count_; i++)
-      {
-        std::cout << value_ << "\n";
+    // case 3: target has just one child
+    if (target_left_child) {
+      if (is_left) {
+        target_root->child_left_ = std::move(target_left_child);
+      } else {
+        target_root->child_right_ = std::move(target_left_child);
       }
-
-      // if exists, recursive call on left child
-      if (child_left_)
-        child_left_->printTree();
-
-      // else try the right side
-      if (child_right_)
-        child_right_->printTree();
+    } else {
+      if (is_left) {
+        target_root->child_left_ = std::move(target_right_child);
+      } else {
+        target_root->child_right_ = std::move(target_right_child);
+      }
     }
 
-    // getters
-    T getValue() const
-    {
-      return value_;
+    return true;
+  }
+
+  Tree<T> *findNode(const T &element) {
+    // return current Node
+    if (element == value_) {
+      return this;
     }
+
+    // search left branch
+    if (child_left_ && element < value_) {
+      return child_left_->findNode(element);
+    }
+
+    // search right branch
+    if (child_right_ && element > value_) {
+      return child_right_->findNode(element);
+    }
+
+    // failed to find Node, return nullptr as fallback value
+    return nullptr;
+  }
+
+  // prints the tree using preorder traversal
+  void printTree() const {
+    // print current value, possible also duplicates
+    for (std::size_t i = 0; i < count_; i++) {
+      std::cout << value_ << "\n";
+    }
+
+    // if exists, recursive call on left child
+    if (child_left_)
+      child_left_->printTree();
+
+    // else try the right side
+    if (child_right_)
+      child_right_->printTree();
+  }
+
+  // getters
+  T getValue() const { return value_; }
 };
 
-int main()
-{
+int main() {
   // initialize tree
   Tree<int> t(2);
 
@@ -214,14 +198,17 @@ int main()
 
   t.printTree();
 
-  Tree<int>* node = t.findNode(3);
+  Tree<int> *node = t.findNode(3);
 
-  std::cout << node->getValue() << "\n";
+  std::cout << "Found node with value: " << node->getValue() << "\n";
 
-  if (!node)
-  {
+  if (!node) {
     std::cout << "failed to find node\n";
   }
+
+  t.deleteNode(3);
+
+  t.printTree();
 
   return 0;
 }
