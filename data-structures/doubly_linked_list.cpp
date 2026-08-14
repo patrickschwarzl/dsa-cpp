cfe1c1c31dc493709b647cad59e7eaa730143483
Patrick Schwarzl <pacoding199@gmail.com>
2026-08-14 17:27:21 +0200

    added printList()

--- data-structures/doubly_linked_list.cpp
+++ data-structures/doubly_linked_list.cpp
@@ -20,31 +20,44 @@ class Node
     std::unique_ptr<Node<T>> child_;
 
   public:
-  // constructor
+    // constructor
     Node(const T &value) : value_(value), parent_(nullptr), child_(nullptr) {}
 
-  // basic operations
-  void addNode(const T &value)
-  {
-    Node *target_node = this;
-
-    while (target_node->child_)
+    // basic operations
+    void addNode(const T &value)
     {
-      target_node = target_node->child_.get();
+      Node *target_node = this;
+
+      while (target_node->child_)
+      {
+        target_node = target_node->child_.get();
+      }
+
+      target_node->child_ = std::make_unique<Node<T>>(value);
+      target_node->child_->parent_ = this;
     }
 
-    target_node->child_ = std::make_unique(Node<T>(value));
-    target_node->child_->parent_ = this;
-  }
+    void printList()
+    {
+      Node<T> *target_node = this;
 
+      std::cout << "Value: " << target_node->value_ << "\n";
 
-  };
+      if (target_node->child_)
+      {
+        target_node->child_->printList();
+      }
+    }
+};
 
 int main()
 {
-  Node<int> List(1);
+  Node<int> list(1);
 
+  list.addNode(10);
+  list.addNode(20);
 
+  list.printList();
 
   return 0;
 }
