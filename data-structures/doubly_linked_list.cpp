6594cee50cd033edf9e65efc99c4a8b8ed8523ac
Patrick Schwarzl <pacoding199@gmail.com>
2026-08-14 17:00:30 +0200

    created basic Node structure

---
+++ data-structures/doubly_linked_list.cpp
@@ -0,0 +1,31 @@
+// DOUBLY LINKED LIST
+// a class oriented doubly linked list implementation that features all basic
+// operations. Every Node is being allocated on the heap using unique pointers
+// for traversing and an additional pointer which points to the Nodes parent.
+// Valid datatypes include std::size_t, int, double, float.
+//
+// TIME COMPLEXITY
+//
+
+#include <iostream>
+#include <memory>
+
+template <typename T>
+class Node
+{
+  // members
+  private:
+
+
+  public:
+
+
+};
+
+int main()
+{
+
+
+
+  return 0;
+}
